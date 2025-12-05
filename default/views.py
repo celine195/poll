2a59bb948ab1210a2b98ee66ef0ee4633588415a
Celine <11335064@dcsh.tp.edu.tw>
2025-12-05 11:45:39 +0800

    完成

---
+++ default/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+from .models import Poll, Option
+from django.views.generic import ListView, DetailView, RedirectView
+from django.urls import reverse
+
+# Create your views here.
+def poll_list(req):
+    polls = Poll.objects.all()
+    return render(req, "default/list.html",{'poll_list':polls,'msg':'Hello!'})
+
+class PollList(ListView):
+    model = Poll
+
+    #應用程式名稱/資料模型_list.html
+    #default/poll_list.html
+
+class  PollView(DetailView):
+    model = Poll
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        option_list = Option.objects.filter(poll_id=self.object.id)
+        ctx['option_list']=option_list
+        return ctx
+    
+    #default/poll_detail.html
+    
+class PollVote(RedirectView):
+    def get_redirect_url(self, *args, **kwargs):
+        option=Option.objects.get(id=self.kwargs['oid'])
+        option.votes+= 1 #option.votes = option.votes +1
+        option.save()
+        #return"/poll/{}/".format(option.poll_id)
+        
+        return reverse('poll_view',args=[option.poll_id])
